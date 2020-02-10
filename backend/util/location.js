b7dd8fe3a36f44aa3aa934da90257e2a886f0b8a
Tomasz Daniel <tomaszz.daniel@gmail.com>
2020-02-11 00:47:42 +0100

    add db

--- backend/util/location.js
+++ backend/util/location.js
@@ -5,16 +5,11 @@ const HttpError = require('../models/http-error');
 const API_KEY = 'AIzaSyCREy__YXCh1lej8haR-4JolE-kZf7rJ7Q';
 
 async function getCoordsForAddress(address) {
-  // return {
-  //   lat: 40.7484474,
-  //   lng: -73.9871516
-  // };
   const response = await axios.get(
     `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
       address
     )}&key=${API_KEY}`
   );
-  console.log(response)
 
   const data = response.data;
 
